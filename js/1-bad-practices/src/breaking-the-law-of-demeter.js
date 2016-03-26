43766f543711527056c826e5bccd795642411346
tfrommen <info@tfrommen.de>
2016-03-26 20:37:28 +0100

    Don't talk to strangers.

--- js/1-bad-practices/src/breaking-the-law-of-demeter.js
+++ js/1-bad-practices/src/breaking-the-law-of-demeter.js
@@ -1,11 +1,11 @@
-function isIdEven( someObject ) {
-	var id = someObject.getOtherObject().getId();
+function isIdEven( otherObject ) {
+	var id = otherObject().getId();
 
 	return ! ( id % 2 );
 }
 
 function filterByEvenId( someObject ) {
-	if ( isIdEven( someObject ) ) {
+	if ( isIdEven( someObject.getOtherObject() ) ) {
 		return someObject;
 	}
 
