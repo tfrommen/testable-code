819767b061a13e9c3d565ff9bc1e96d90c20d432
tfrommen <info@tfrommen.de>
2016-03-26 20:45:56 +0100

    Add missing code example.

---
+++ php/1-bad-practices/src/insane-complexity.php
@@ -0,0 +1,15 @@
+<?php # -*- coding: utf-8 -*-
+
+class Processor {
+
+	public function process( $data ) {
+
+		// 16 lines full of different checks, a lot of if-else, ...
+
+		// 23 lines full of data preparing, switch-case all over, ...
+
+		// 42 lines full of data processing...
+
+		// 8 lines full of post-processing, if-elseif-else again...
+	}
+}
